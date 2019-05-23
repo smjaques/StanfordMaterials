f1019990931a2f706f268dfd5eeedfba70f0fa48
smjaques <sjaques@calpoly.edu>
2019-05-22 18:22:57 -0700

    from a property page code

---
+++ fromProperty.js
@@ -0,0 +1,99 @@
+$w.onReady(function () {
+	$w('#costbox').hide();
+	$w('#costtext').hide();
+	$w('#densitybox').hide();
+	$w('#densityText').hide();
+	$w('#ductilebox').hide();
+	$w('#ductiletext').hide();
+	$w('#conductbox').hide();
+	$w('#conductText').hide();
+	$w('#foodbox').hide();
+	$w('#foodtext').hide();
+	$w('#chemicalbox').hide();
+	$w('#chemicaltext').hide();
+	$w('#corrosionbox').hide();
+	$w('#corrosiontext').hide();
+	$w('#hardbox').hide();
+	$w('#hardtext').hide();
+});
+
+export function cost_mouseIn(event) {
+	$w('#costbox').show();
+	$w('#costtext').show();
+}
+
+export function costbox_mouseOut(event) {
+	$w('#costbox').hide();
+	$w('#costtext').hide();
+}
+
+export function density_mouseIn(event) {
+	$w('#densitybox').show();
+	$w('#densityText').show();
+}
+
+export function densitybox_mouseOut(event) {
+	$w('#densitybox').hide();
+	$w('#densityText').hide();
+}
+
+export function ductility_mouseIn(event) {
+	$w('#ductilebox').show();
+	$w('#ductiletext').show();
+}
+
+export function ductilebox_mouseOut(event) {
+	$w('#ductilebox').hide();
+	$w('#ductiletext').hide();
+}
+
+export function conductivity_mouseIn(event) {
+	$w('#conductbox').show();
+	$w('#conductText').show();
+}
+
+export function conductbox_mouseOut(event) {
+	$w('#conductbox').hide();
+	$w('#conductText').hide();
+}
+
+export function foodbox_mouseOut(event) {
+	$w('#foodbox').hide();
+	$w('#foodtext').hide(); 
+}
+
+export function food_mouseIn(event) {
+	$w('#foodbox').show();
+	$w('#foodtext').show();
+
+}
+
+export function chemical_mouseIn(event) {
+	$w('#chemicalbox').show();
+	$w('#chemicaltext').show();
+}
+
+export function chemicalbox_mouseOut(event) {
+	$w('#chemicalbox').hide();
+	$w('#chemicaltext').hide();
+}
+
+export function corrosion_mouseIn(event) {
+	$w('#corrosionbox').show();
+	$w('#corrosiontext').show();
+}
+
+export function corrosionbox_mouseOut(event) {
+	$w('#corrosionbox').hide();
+	$w('#corrosiontext').hide();
+}
+
+export function hardness_mouseIn(event) {
+	$w('#hardbox').show();
+	$w('#hardtext').show();
+}
+
+export function hardbox_mouseOut(event) {
+	$w('#hardbox').hide();
+	$w('#hardtext').hide();
+}
